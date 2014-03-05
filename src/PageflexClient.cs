3ca3555f11bae75e22adefd199366665ce7a8bc0
Danny Graham <good.midget@gmail.com>
2014-03-05 13:04:05 -0800

    fixes console write

--- src/PageflexClient.cs
+++ src/PageflexClient.cs
@@ -6,7 +6,11 @@ public class PageflexClient {
 	public static void Main() {
 		Console.WriteLine("Init");
 
+		Console.WriteLine("ServerHelpers connecting to Pageflex Server...");
 		ServerHelpers helpers = new ServerHelpers("locahost", 8008);
+		Console.WriteLine("Connected");
+
+		Console.WriteLine("ServerHelpers fetching queue length...");
 		int QueueLength = helpers.GetQueueLength("pfclient-test");
 		Console.WriteLine("Queue Length:\t{0,8:c}", QueueLength);
 	}
